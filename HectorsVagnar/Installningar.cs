73e57753e5a2ba49b858121b8b7f7ec2ed7e98b5
Hermansson <daniel.hermansson@gmail.com>
2023-06-16 09:22:35 +0200

    Intentering i mallen

--- HectorsVagnar/Installningar.cs
+++ HectorsVagnar/Installningar.cs
@@ -5,17 +5,16 @@ namespace HectorsVagnar
     public class Installningar
     {
         public string Epost { get; set; } = string.Empty;
-        public string MailMall { get; set; } = @"
-            Hej!
-            {lastatEllerEj}
-            
-            {lok}
-            
-            {vagnar}
-            
-            Hälsningar
-            Förnamn Efternamn
-            Förare Stad 
-            123-456 78 90";
+        public string MailMall { get; set; } = @"Hej!
+{lastatEllerEj}
+
+{lok}
+
+{vagnar}
+
+Hälsningar
+Förnamn Efternamn
+Förare Stad 
+123-456 78 90";
     }
 }
